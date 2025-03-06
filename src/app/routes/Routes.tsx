b45101b188ced0034463ba70b3858a2a6b59a81a
Oleg Grunski <oleggr578@gmail.com>
2025-03-06 22:02:25 +0100

    add checkout page route to application

--- src/app/routes/Routes.tsx
+++ src/app/routes/Routes.tsx
@@ -9,6 +9,7 @@ import App from '../layout/App';
 import ServerErrors from '../errors/ServerErrors';
 import NotFound from '../errors/NotFound';
 import BasketPage from '../../features/basket/BasketPage';
+import CheckoutPage from '../../features/checkout/CheckoutPage';
 
 export const router = createBrowserRouter(
 	[
@@ -23,6 +24,7 @@ export const router = createBrowserRouter(
 				{ path: '/contact', element: <ContactPage /> },
 				{ path: '/basket', element: <BasketPage /> },
 				{ path: '/server-error', element: <ServerErrors /> },
+				{ path: '/checkout', element: <CheckoutPage /> },
 				{ path: '/not-found', element: <NotFound /> },
 				{ path: '*', element: <Navigate replace to="/not-found" /> },
 			],
