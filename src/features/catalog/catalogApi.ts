7da32d1b8c8aa0357911076cfa3403d61db39ad6
Oleg Grunski <oleggr578@gmail.com>
2025-03-14 00:19:06 +0100

    add fetchFilters endpoint to catalog API for product filtering options

--- src/features/catalog/catalogApi.ts
+++ src/features/catalog/catalogApi.ts
@@ -1,19 +1,24 @@
 import { baseQueryWithErrorHandling } from './../../app/api/baseApi';
-import {createApi} from '@reduxjs/toolkit/query/react';
+import { createApi } from '@reduxjs/toolkit/query/react';
 import { Product } from '../../models/product';
 
 export const catalogApi = createApi({
-    reducerPath: 'catalogApi',
-    baseQuery: baseQueryWithErrorHandling,
-    endpoints: (builder) => ({
-        fetchProducts: builder.query<Product[], void>({
-            query: () => 'products'
-        }),
+	reducerPath: 'catalogApi',
+	baseQuery: baseQueryWithErrorHandling,
+	endpoints: (builder) => ({
+		fetchProducts: builder.query<Product[], void>({
+			query: () => 'products',
+		}),
 
-        fetchProductDetails: builder.query<Product, string>({
-            query: (productId) => `products/${productId}`
-        }),
-    }),
-})
+		fetchProductDetails: builder.query<Product, string>({
+			query: (productId) => `products/${productId}`,
+		}),
 
-export const { useFetchProductsQuery, useFetchProductDetailsQuery } = catalogApi;
+		fetchFilters: builder.query<{ brands: string[]; types: string[] }, void>({
+			query: () => 'products/filters',
+		}),
+	}),
+});
+
+export const { useFetchProductsQuery, useFetchProductDetailsQuery, useFetchFiltersQuery } =
+	catalogApi;
