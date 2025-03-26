de812f2f7e8a903e46c20f52f4267b2832f7afd5
Oleg Grunski <oleggr578@gmail.com>
2025-03-27 00:01:46 +0100

    update fetchProducts endpoint in catalog API to accept product parameters for enhanced querying

--- src/features/catalog/catalogApi.ts
+++ src/features/catalog/catalogApi.ts
@@ -1,13 +1,21 @@
 import { baseQueryWithErrorHandling } from './../../app/api/baseApi';
 import { createApi } from '@reduxjs/toolkit/query/react';
 import { Product } from '../../models/product';
+import { ProductParams } from '../../models/productParams';
+import { prodParams } from '../../lib/util';
 
 export const catalogApi = createApi({
 	reducerPath: 'catalogApi',
 	baseQuery: baseQueryWithErrorHandling,
 	endpoints: (builder) => ({
-		fetchProducts: builder.query<Product[], void>({
-			query: () => 'products',
+		fetchProducts: builder.query<Product[], ProductParams>({
+			query: (productParams) => {
+
+				return {
+					url: 'products',
+					params: prodParams(productParams),
+				};
+			},
 		}),
 
 		fetchProductDetails: builder.query<Product, string>({
