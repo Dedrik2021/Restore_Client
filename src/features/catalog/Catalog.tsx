cffbaeeb1c240f59d4bcd1d540e5f5c581538185
Oleg Grunski <oleggr578@gmail.com>
2025-03-28 00:15:59 +0100

    integrate Filters component into Catalog for enhanced product filtering and update fetch logic to utilize product parameters from the store

--- src/features/catalog/Catalog.tsx
+++ src/features/catalog/Catalog.tsx
@@ -1,17 +1,34 @@
-import ProductList from "./ProductList";
-import { useFetchProductsQuery } from "./catalogApi";
-
+import { Grid2, Pagination } from '@mui/material';
+import ProductList from './ProductList';
+import { useFetchProductsQuery } from './catalogApi';
+import Filters from './Filters';
+import { useAppSelector } from '../../app/store/store';
 
 const Catalog = () => {
-	const {data, isLoading} = useFetchProductsQuery();
+	const productParams = useAppSelector((state) => state.catalog);
+	const { data, isLoading } = useFetchProductsQuery(productParams);
 
 	if (isLoading || !data) return <div>Loading...</div>;
 
-    return ( 
-        <>
-            <ProductList products={data} />
-        </>
-    );
-}
+	return (
+		<Grid2 container spacing={4}>
+			<Grid2 size={3}>
+				<Filters />
+			</Grid2>
+			<Grid2 size={9}>
+				<ProductList products={data} />
+				{/* <Pagination
+					color="secondary"
+					size="large"
+					count={data.pagination.totalPages}
+					page={data.pagination.currentPage}
+					// onChange={(e, value) => {
+					// 	dispatch(setPageNumber(value));
+					// }}
+				/> */}
+			</Grid2>
+		</Grid2>
+	);
+};
 
 export default Catalog;
