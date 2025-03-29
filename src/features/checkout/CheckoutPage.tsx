3e6fe397498cca187aecb021f72976a3b6905997
Oleg Grunski <oleggr578@gmail.com>
2025-03-30 00:50:16 +0100

    create CheckoutPage component to display authorization message for users

---
+++ src/features/checkout/CheckoutPage.tsx
@@ -0,0 +1,11 @@
+import { Typography } from "@mui/material";
+
+const CheckoutPage = () => {
+    return ( 
+        <Typography>
+            Only authorized user should be able to see this
+        </Typography>
+    );
+}
+
+export default CheckoutPage;
