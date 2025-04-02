5ee4d95f0f2172092613ebfd739731c60390f44d
Oleg Grunski <oleggr578@gmail.com>
2025-04-02 23:36:09 +0200

    add prodParams utility function for filtering out undefined, empty, and null values from objects

--- src/lib/util.ts
+++ src/lib/util.ts
@@ -1,3 +1,11 @@
 export const currencyFormat = (amount: number) => {
-    return '$' + (amount / 100).toFixed(2);
-}
+	return '$' + (amount / 100).toFixed(2);
+};
+
+export const prodParams = (value: object) => {
+	return Object.fromEntries(
+		Object.entries(value).filter(
+			([_, v]) => v !== undefined && v !== '' && v !== null && v.length !== 0,
+		),
+	);
+};
